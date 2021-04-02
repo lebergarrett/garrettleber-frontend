3973739f7d73a6c9e0acdff74c2ae535b08b86f7
imkumpy <lebergarrett@gmail.com>
2021-04-02 14:31:50 -0400

    Fixed avatar image and api url

--- counter.js
+++ counter.js
@@ -1,3 +1,3 @@
-fetch ('https://wvmekkxrt8.execute-api.us-east-1.amazonaws.com/prod/getcount')
+fetch ('https://44ts8mx4qf.execute-api.us-east-1.amazonaws.com/prod/getcount')
 .then(x => x.text())
 .then(y => document.getElementById("hits").innerHTML = y);
